fac5b8ed1173accf657ddf295310f59d847aae37
Hkk258 <harishkarthick.2208007@srec.ac.in>
2026-04-13 20:39:49 +0530

    Add files via upload

---
+++ Multibody_Dynamics/SImulink Model/testWOblade/testWOblade_DataFile.m
@@ -0,0 +1,326 @@
+% Simscape(TM) Multibody(TM) version: 25.2
+
+% This is a model data file derived from a Simscape Multibody Import XML file using the smimport function.
+% The data in this file sets the block parameter values in an imported Simscape Multibody model.
+% For more information on this file, see the smimport function help page in the Simscape Multibody documentation.
+% You can modify numerical values, but avoid any other changes to this file.
+% Do not add code to this file. Do not edit the physical units shown in comments.
+
+%%%VariableName:smiData
+
+
+%============= RigidTransform =============%
+
+%Initialize the RigidTransform structure array by filling in null values.
+smiData.RigidTransform(25).translation = [0.0 0.0 0.0];
+smiData.RigidTransform(25).angle = 0.0;
+smiData.RigidTransform(25).axis = [0.0 0.0 0.0];
+smiData.RigidTransform(25).ID = "";
+
+%Translation Method - Cartesian
+%Rotation Method - Arbitrary Axis
+smiData.RigidTransform(1).translation = [-6.6000000000000005 0.39999999999999969 -1.6308603780117312];  % cm
+smiData.RigidTransform(1).angle = 2.0943951023931953;  % rad
+smiData.RigidTransform(1).axis = [-0.57735026918962584 -0.57735026918962584 0.57735026918962584];
+smiData.RigidTransform(1).ID = "B[CF_Plate-1:-:FeatherShaftClamp-1]";
+
+%Translation Method - Cartesian
+%Rotation Method - Arbitrary Axis
+smiData.RigidTransform(2).translation = [1.5999999999999972 -0.999999999999998 -1.4999999999999685];  % cm
+smiData.RigidTransform(2).angle = 3.1415926535897931;  % rad
+smiData.RigidTransform(2).axis = [0.70710678118654757 0.70710678118654757 4.8572257327350586e-17];
+smiData.RigidTransform(2).ID = "F[CF_Plate-1:-:FeatherShaftClamp-1]";
+
+%Translation Method - Cartesian
+%Rotation Method - Arbitrary Axis
+smiData.RigidTransform(3).translation = [6.6000000000004517 0.39999999999999969 -1.6308603780117312];  % cm
+smiData.RigidTransform(3).angle = 2.0943951023931953;  % rad
+smiData.RigidTransform(3).axis = [0.57735026918962584 0.57735026918962584 0.57735026918962584];
+smiData.RigidTransform(3).ID = "B[CF_Plate-1:-:FeatherShaftClamp-2]";
+
+%Translation Method - Cartesian
+%Rotation Method - Arbitrary Axis
+smiData.RigidTransform(4).translation = [1.5999999999999972 -1.0000000000000009 1.4999999999999925];  % cm
+smiData.RigidTransform(4).angle = 1.5707963267948966;  % rad
+smiData.RigidTransform(4).axis = [-6.8691545067415147e-17 -6.8691545067415147e-17 1];
+smiData.RigidTransform(4).ID = "F[CF_Plate-1:-:FeatherShaftClamp-2]";
+
+%Translation Method - Cartesian
+%Rotation Method - Arbitrary Axis
+smiData.RigidTransform(5).translation = [0 24 0];  % cm
+smiData.RigidTransform(5).angle = 2.0943951023931953;  % rad
+smiData.RigidTransform(5).axis = [0.57735026918962584 -0.57735026918962584 0.57735026918962584];
+smiData.RigidTransform(5).ID = "B[RodWithBase-1:-:CF_Plate-1]";
+
+%Translation Method - Cartesian
+%Rotation Method - Arbitrary Axis
+smiData.RigidTransform(6).translation = [0 18.699999999999999 0];  % cm
+smiData.RigidTransform(6).angle = 2.0943951023931957;  % rad
+smiData.RigidTransform(6).axis = [0.57735026918962584 -0.57735026918962584 0.57735026918962562];
+smiData.RigidTransform(6).ID = "F[RodWithBase-1:-:CF_Plate-1]";
+
+%Translation Method - Cartesian
+%Rotation Method - Arbitrary Axis
+smiData.RigidTransform(7).translation = [-1.4999999999999987 1.0000000000000009 -1.5];  % cm
+smiData.RigidTransform(7).angle = 2.0943951023931953;  % rad
+smiData.RigidTransform(7).axis = [-0.57735026918962584 -0.57735026918962584 -0.57735026918962584];
+smiData.RigidTransform(7).ID = "B[FeatherShaftClamp-1:-:CF_Plate-2]";
+
+%Translation Method - Cartesian
+%Rotation Method - Arbitrary Axis
+smiData.RigidTransform(8).translation = [-6.6000000000000361 0 1.4691396219882646];  % cm
+smiData.RigidTransform(8).angle = 2.0943951023931957;  % rad
+smiData.RigidTransform(8).axis = [-0.57735026918962584 -0.57735026918962584 -0.57735026918962562];
+smiData.RigidTransform(8).ID = "F[FeatherShaftClamp-1:-:CF_Plate-2]";
+
+%Translation Method - Cartesian
+%Rotation Method - Arbitrary Axis
+smiData.RigidTransform(9).translation = [1.5000000000000007 0.9000000000000008 1.4999999999999987];  % cm
+smiData.RigidTransform(9).angle = 0;  % rad
+smiData.RigidTransform(9).axis = [0 0 0];
+smiData.RigidTransform(9).ID = "B[FeatherShaftClamp-2:-:CF_Plate-2]";
+
+%Translation Method - Cartesian
+%Rotation Method - Arbitrary Axis
+smiData.RigidTransform(10).translation = [6.6000000000004615 -0.09999999999999698 -1.5308603780117362];  % cm
+smiData.RigidTransform(10).angle = 2.0943951023931953;  % rad
+smiData.RigidTransform(10).axis = [0.57735026918962584 0.57735026918962584 0.57735026918962584];
+smiData.RigidTransform(10).ID = "F[FeatherShaftClamp-2:-:CF_Plate-2]";
+
+%Translation Method - Cartesian
+%Rotation Method - Arbitrary Axis
+smiData.RigidTransform(11).translation = [0 24 0];  % cm
+smiData.RigidTransform(11).angle = 2.0943951023931953;  % rad
+smiData.RigidTransform(11).axis = [0.57735026918962584 -0.57735026918962584 0.57735026918962584];
+smiData.RigidTransform(11).ID = "B[RodWithBase-1:-:CF_Plate-2]";
+
+%Translation Method - Cartesian
+%Rotation Method - Arbitrary Axis
+smiData.RigidTransform(12).translation = [0 16.300000000000001 0];  % cm
+smiData.RigidTransform(12).angle = 2.0943951023931953;  % rad
+smiData.RigidTransform(12).axis = [0.57735026918962584 -0.57735026918962584 0.57735026918962584];
+smiData.RigidTransform(12).ID = "F[RodWithBase-1:-:CF_Plate-2]";
+
+%Translation Method - Cartesian
+%Rotation Method - Arbitrary Axis
+smiData.RigidTransform(13).translation = [1.5000000000000007 0.9000000000000008 1.4999999999999987];  % cm
+smiData.RigidTransform(13).angle = 0;  % rad
+smiData.RigidTransform(13).axis = [0 0 0];
+smiData.RigidTransform(13).ID = "B[FeatherShaftClamp-2:-:Blade Grip-1]";
+
+%Translation Method - Cartesian
+%Rotation Method - Arbitrary Axis
+smiData.RigidTransform(14).translation = [1.4633843780105786 -0.59161272848884361 -0.5000000000000131];  % cm
+smiData.RigidTransform(14).angle = 8.3337181474919236e-16;  % rad
+smiData.RigidTransform(14).axis = [-0.060785495196087981 0.99815085211292909 -2.528162315414481e-17];
+smiData.RigidTransform(14).ID = "F[FeatherShaftClamp-2:-:Blade Grip-1]";
+
+%Translation Method - Cartesian
+%Rotation Method - Arbitrary Axis
+smiData.RigidTransform(15).translation = [1.5000000000000009 0.9000000000000008 -1.5];  % cm
+smiData.RigidTransform(15).angle = 3.1415926535897931;  % rad
+smiData.RigidTransform(15).axis = [1 0 0];
+smiData.RigidTransform(15).ID = "B[FeatherShaftClamp-1:-:Blade Grip-2]";
+
+%Translation Method - Cartesian
+%Rotation Method - Arbitrary Axis
+smiData.RigidTransform(16).translation = [-1.4757370823607463 -0.61074494212412533 -0.49999999999997052];  % cm
+smiData.RigidTransform(16).angle = 1.3335863718605435e-15;  % rad
+smiData.RigidTransform(16).axis = [0.86076140104685206 0.50900865460997868 2.9214543430927187e-16];
+smiData.RigidTransform(16).ID = "F[FeatherShaftClamp-1:-:Blade Grip-2]";
+
+%Translation Method - Cartesian
+%Rotation Method - Arbitrary Axis
+smiData.RigidTransform(17).translation = [0 0 3.2500000000000013];  % cm
+smiData.RigidTransform(17).angle = 3.1415926535897931;  % rad
+smiData.RigidTransform(17).axis = [1 0 0];
+smiData.RigidTransform(17).ID = "B[FeatherShaft-1:-:Blade Grip-1]";
+
+%Translation Method - Cartesian
+%Rotation Method - Arbitrary Axis
+smiData.RigidTransform(18).translation = [-2.2204460492503131e-15 -1.5499999999999996 3.0000000000000115];  % cm
+smiData.RigidTransform(18).angle = 3.1415926535897931;  % rad
+smiData.RigidTransform(18).axis = [-1 5.9391735921454446e-34 -4.4602229440179872e-17];
+smiData.RigidTransform(18).ID = "F[FeatherShaft-1:-:Blade Grip-1]";
+
+%Translation Method - Cartesian
+%Rotation Method - Arbitrary Axis
+smiData.RigidTransform(19).translation = [0 0 0.44999999999999973];  % cm
+smiData.RigidTransform(19).angle = 0;  % rad
+smiData.RigidTransform(19).axis = [0 0 0];
+smiData.RigidTransform(19).ID = "B[FeatherShaftClamp-2:-:FeatherShaft-1]";
+
+%Translation Method - Cartesian
+%Rotation Method - Arbitrary Axis
+smiData.RigidTransform(20).translation = [-5.5511151231257827e-17 4.9960036108132044e-16 -1.3000000000000198];  % cm
+smiData.RigidTransform(20).angle = 9.1687971987880106e-16;  % rad
+smiData.RigidTransform(20).axis = [-0.98909725156619566 -0.14726380052204882 6.6775534136987378e-17];
+smiData.RigidTransform(20).ID = "F[FeatherShaftClamp-2:-:FeatherShaft-1]";
+
+%Translation Method - Cartesian
+%Rotation Method - Arbitrary Axis
+smiData.RigidTransform(21).translation = [0 0 3.2500000000000009];  % cm
+smiData.RigidTransform(21).angle = 3.1415926535897931;  % rad
+smiData.RigidTransform(21).axis = [1 0 0];
+smiData.RigidTransform(21).ID = "B[FeatherShaft-2:-:Blade Grip-2]";
+
+%Translation Method - Cartesian
+%Rotation Method - Arbitrary Axis
+smiData.RigidTransform(22).translation = [-6.7723604502134549e-15 -1.5499999999999985 -3.4999999999999694];  % cm
+smiData.RigidTransform(22).angle = 1.1822430030057124e-15;  % rad
+smiData.RigidTransform(22).axis = [0.95623068803821132 0.29261386032443698 1.6539955150804078e-16];
+smiData.RigidTransform(22).ID = "F[FeatherShaft-2:-:Blade Grip-2]";
+
+%Translation Method - Cartesian
+%Rotation Method - Arbitrary Axis
+smiData.RigidTransform(23).translation = [0 0 -0.44999999999999973];  % cm
+smiData.RigidTransform(23).angle = 3.1415926535897931;  % rad
+smiData.RigidTransform(23).axis = [1 0 0];
+smiData.RigidTransform(23).ID = "B[FeatherShaftClamp-1:-:FeatherShaft-2]";
+
+%Translation Method - Cartesian
+%Rotation Method - Arbitrary Axis
+smiData.RigidTransform(24).translation = [1.7763568394002505e-15 0 1.3000000000000052];  % cm
+smiData.RigidTransform(24).angle = 3.1415926535897931;  % rad
+smiData.RigidTransform(24).axis = [1 -6.8555614406835666e-34 -1.6653345369377348e-16];
+smiData.RigidTransform(24).ID = "F[FeatherShaftClamp-1:-:FeatherShaft-2]";
+
+%Translation Method - Cartesian
+%Rotation Method - Arbitrary Axis
+smiData.RigidTransform(25).translation = [0 0 0];  % cm
+smiData.RigidTransform(25).angle = 0;  % rad
+smiData.RigidTransform(25).axis = [0 0 0];
+smiData.RigidTransform(25).ID = "RootGround[RodWithBase-1]";
+
+
+%============= Solid =============%
+%Center of Mass (CoM) %Moments of Inertia (MoI) %Product of Inertia (PoI)
+
+%Initialize the Solid structure array by filling in null values.
+smiData.Solid(5).mass = 0.0;
+smiData.Solid(5).CoM = [0.0 0.0 0.0];
+smiData.Solid(5).MoI = [0.0 0.0 0.0];
+smiData.Solid(5).PoI = [0.0 0.0 0.0];
+smiData.Solid(5).color = [0.0 0.0 0.0];
+smiData.Solid(5).opacity = 0.0;
+smiData.Solid(5).ID = "";
+
+%Inertia Type - Custom
+%Visual Properties - Simple
+smiData.Solid(1).mass = 0.0018378317023500295;  % kg
+smiData.Solid(1).CoM = [0 0 0];  % mm
+smiData.Solid(1).MoI = [0.66590768681816048 0.66590768681816048 0.037675549898175613];  % kg*mm^2
+smiData.Solid(1).PoI = [0 0 0];  % kg*mm^2
+smiData.Solid(1).color = [0.792156862745098 0.81960784313725488 0.93333333333333335];
+smiData.Solid(1).opacity = 1;
+smiData.Solid(1).ID = "FeatherShaft*:*Default";
+
+%Inertia Type - Custom
+%Visual Properties - Simple
+smiData.Solid(2).mass = 0.036470780732673007;  % kg
+smiData.Solid(2).CoM = [0 -15.719838996430497 35.90986634933369];  % mm
+smiData.Solid(2).MoI = [27.212256283255694 24.728633333620344 6.9012762530634921];  % kg*mm^2
+smiData.Solid(2).PoI = [-0.20773753116189719 0 0];  % kg*mm^2
+smiData.Solid(2).color = [0.792156862745098 0.81960784313725488 0.93333333333333335];
+smiData.Solid(2).opacity = 1;
+smiData.Solid(2).ID = "Blade Grip*:*Default";
+
+%Inertia Type - Custom
+%Visual Properties - Simple
+smiData.Solid(3).mass = 0.03214791762418432;  % kg
+smiData.Solid(3).CoM = [0 89.687335092348235 0];  % mm
+smiData.Solid(3).MoI = [207.36574873617354 5.3740036808417146 207.36574873617354];  % kg*mm^2
+smiData.Solid(3).PoI = [0 0 0];  % kg*mm^2
+smiData.Solid(3).color = [0.792156862745098 0.81960784313725488 0.93333333333333335];
+smiData.Solid(3).opacity = 1;
+smiData.Solid(3).ID = "RodWithBase*:*Default";
+
+%Inertia Type - Custom
+%Visual Properties - Simple
+smiData.Solid(4).mass = 0.014683930129923634;  % kg
+smiData.Solid(4).CoM = [0 0 0];  % mm
+smiData.Solid(4).MoI = [1.6259174865073451 2.6203691667708524 2.1544150074081019];  % kg*mm^2
+smiData.Solid(4).PoI = [0 0 0];  % kg*mm^2
+smiData.Solid(4).color = [0.792156862745098 0.81960784313725488 0.93333333333333335];
+smiData.Solid(4).opacity = 1;
+smiData.Solid(4).ID = "FeatherShaftClamp*:*Default";
+
+%Inertia Type - Custom
+%Visual Properties - Simple
+smiData.Solid(5).mass = 0.020376151387795291;  % kg
+smiData.Solid(5).CoM = [2.6001135480945333e-12 2.0000000000000004 -0.31180016296571189];  % mm
+smiData.Solid(5).MoI = [4.0501557430339643 33.26709410751053 29.27127476817736];  % kg*mm^2
+smiData.Solid(5).PoI = [0 0 0];  % kg*mm^2
+smiData.Solid(5).color = [0.792156862745098 0.81960784313725488 0.93333333333333335];
+smiData.Solid(5).opacity = 1;
+smiData.Solid(5).ID = "CF_Plate*:*Default";
+
+
+%============= Joint =============%
+%X Revolute Primitive (Rx) %Y Revolute Primitive (Ry) %Z Revolute Primitive (Rz)
+%X Prismatic Primitive (Px) %Y Prismatic Primitive (Py) %Z Prismatic Primitive (Pz) %Spherical Primitive (S)
+%Constant Velocity Primitive (CV) %Lead Screw Primitive (LS)
+%Position Target (Pos)
+
+%Initialize the CylindricalJoint structure array by filling in null values.
+smiData.CylindricalJoint(3).Rz.Pos = 0.0;
+smiData.CylindricalJoint(3).Pz.Pos = 0.0;
+smiData.CylindricalJoint(3).ID = "";
+
+smiData.CylindricalJoint(1).Rz.Pos = -81.470585900770701;  % deg
+smiData.CylindricalJoint(1).Pz.Pos = 0;  % cm
+smiData.CylindricalJoint(1).ID = "[RodWithBase-1:-:CF_Plate-2]";
+
+smiData.CylindricalJoint(2).Rz.Pos = -93.62421140142915;  % deg
+smiData.CylindricalJoint(2).Pz.Pos = 0;  % cm
+smiData.CylindricalJoint(2).ID = "[FeatherShaft-1:-:Blade Grip-1]";
+
+smiData.CylindricalJoint(3).Rz.Pos = -178.48842916852684;  % deg
+smiData.CylindricalJoint(3).Pz.Pos = 0;  % cm
+smiData.CylindricalJoint(3).ID = "[FeatherShaft-2:-:Blade Grip-2]";
+
+
+%Initialize the PlanarJoint structure array by filling in null values.
+smiData.PlanarJoint(4).Rz.Pos = 0.0;
+smiData.PlanarJoint(4).Px.Pos = 0.0;
+smiData.PlanarJoint(4).Py.Pos = 0.0;
+smiData.PlanarJoint(4).ID = "";
+
+%This joint has been chosen as a cut joint. Simscape Multibody treats cut joints as algebraic constraints to solve closed kinematic loops. The imported model does not use the state target data for this joint.
+smiData.PlanarJoint(1).Rz.Pos = -89.999999999999986;  % deg
+smiData.PlanarJoint(1).Px.Pos = 0;  % cm
+smiData.PlanarJoint(1).Py.Pos = 0;  % cm
+smiData.PlanarJoint(1).ID = "[FeatherShaftClamp-1:-:CF_Plate-2]";
+
+%This joint has been chosen as a cut joint. Simscape Multibody treats cut joints as algebraic constraints to solve closed kinematic loops. The imported model does not use the state target data for this joint.
+smiData.PlanarJoint(2).Rz.Pos = 89.999999999999986;  % deg
+smiData.PlanarJoint(2).Px.Pos = 0;  % cm
+smiData.PlanarJoint(2).Py.Pos = 0;  % cm
+smiData.PlanarJoint(2).ID = "[FeatherShaftClamp-2:-:CF_Plate-2]";
+
+%This joint has been chosen as a cut joint. Simscape Multibody treats cut joints as algebraic constraints to solve closed kinematic loops. The imported model does not use the state target data for this joint.
+smiData.PlanarJoint(3).Rz.Pos = -2.2574941462956954;  % deg
+smiData.PlanarJoint(3).Px.Pos = 0;  % cm
+smiData.PlanarJoint(3).Py.Pos = 0;  % cm
+smiData.PlanarJoint(3).ID = "[FeatherShaftClamp-2:-:Blade Grip-1]";
+
+%This joint has been chosen as a cut joint. Simscape Multibody treats cut joints as algebraic constraints to solve closed kinematic loops. The imported model does not use the state target data for this joint.
+smiData.PlanarJoint(4).Rz.Pos = -178.48842916852684;  % deg
+smiData.PlanarJoint(4).Px.Pos = 0;  % cm
+smiData.PlanarJoint(4).Py.Pos = 0;  % cm
+smiData.PlanarJoint(4).ID = "[FeatherShaftClamp-1:-:Blade Grip-2]";
+
+
+%Initialize the RevoluteJoint structure array by filling in null values.
+smiData.RevoluteJoint(3).Rz.Pos = 0.0;
+smiData.RevoluteJoint(3).ID = "";
+
+smiData.RevoluteJoint(1).Rz.Pos = -81.470585900770672;  % deg
+smiData.RevoluteJoint(1).ID = "[RodWithBase-1:-:CF_Plate-1]";
+
+smiData.RevoluteJoint(2).Rz.Pos = -95.881705547724835;  % deg
+smiData.RevoluteJoint(2).ID = "[FeatherShaftClamp-2:-:FeatherShaft-1]";
+
+smiData.RevoluteJoint(3).Rz.Pos = -6.3259319564544026e-15;  % deg
+smiData.RevoluteJoint(3).ID = "[FeatherShaftClamp-1:-:FeatherShaft-2]";
+
